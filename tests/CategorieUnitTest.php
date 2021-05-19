2ec88be2d6f40179e1662aab3bc82084fd24bb20
Ayoub mchichi <a.mchichi@laposte.net>
2021-05-19 02:10:05 +0200

    need new file for test-unit

---
+++ tests/CategorieUnitTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class CategorieUnitTest extends TestCase
+{
+    public function testIsTrue(): void
+    {
+        $categorie = new Categori
+    }
+}
---
+++ tests/UserUnitTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use Monolog\Test\TestCase;
+
+class UserUnitTest extends TestCase
+{
+    public function testSomething(): void
+    {
+
+    }
+}
