7ad5d78afc95cc703c8cfa8c0c75cfd594dd207c
Ayoub mchichi <a.mchichi@laposte.net>
2021-05-21 03:35:33 +0200

    fixing some bugs related to the doctrine + add faker and generate some fixtures

--- tests/UserUnitTest.php
+++ tests/UserUnitTest.php
@@ -14,16 +14,12 @@ class UserUnitTest extends TestCase
         $user->setEmail('true@test.com')
             ->setPrenom('prenom')
             ->setNom('nom')
-            ->setPassword('password')
-            ->setAPropos('a propos')
-            ->setInstagram('instagram');
+            ->setPassword('password');
 
         $this->assertTrue($user->getEmail() === 'true@test.com');
         $this->assertTrue($user->getPrenom() === 'prenom');
         $this->assertTrue($user->getNom() === 'nom');
         $this->assertTrue($user->getPassword() === 'password');
-        $this->assertTrue($user->getAPropos() === 'a propos');
-        $this->assertTrue($user->getInstagram() === 'instagram');
 
     }
 
@@ -34,16 +30,12 @@ class UserUnitTest extends TestCase
         $user->setEmail('true@test.com')
             ->setPrenom('prenom')
             ->setNom('nom')
-            ->setPassword('password')
-            ->setAPropos('a propos')
-            ->setInstagram('instagram');
+            ->setPassword('password');
 
         $this->assertFalse($user->getEmail() === 'false@test.com');
         $this->assertFalse($user->getPrenom() === 'false');
         $this->assertFalse($user->getNom() === 'false');
         $this->assertFalse($user->getPassword() === 'false');
-        $this->assertFalse($user->getAPropos() === 'false');
-        $this->assertFalse($user->getInstagram() === 'false');
 
     }
 
@@ -55,8 +47,6 @@ class UserUnitTest extends TestCase
         $this->assertEmpty($user->getPrenom());
         $this->assertEmpty($user->getNom());
         $this->assertEmpty($user->getPassword());
-        $this->assertEmpty($user->getAPropos());
-        $this->assertEmpty($user->getInstagram());
 
     }
 }
