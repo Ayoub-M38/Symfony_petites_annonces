5d92abd94d207fbc38005cb0dd0cea052d12deaf
Ayoub mchichi <a.mchichi@laposte.net>
2021-05-21 09:30:18 +0200

    add 6 annonces + last 4 blogposts

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\AnnonceRepository;
+use App\Repository\BlogpostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +13,11 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(AnnonceRepository $annonceRepository, BlogpostRepository $blogpostRepository): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'annonces' => $annonceRepository->lastTree(),
+            'blogposts' => $blogpostRepository->lastTree(),
         ]);
     }
 }
